c0017ff49133dd88ec96e5daa8f1f32bfc00833b
AustinT33 <austintumlinson@gmail.com>
2019-12-06 14:08:37 -0600

    finalized

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const moviedex = require('./moviedex.json');
 
 const app = express();
 
-app.use(morgan('dev'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(helmet());
 app.use(cors());
 app.use(validateBearerToken);
@@ -53,7 +54,17 @@ function handleGetMovie(req, res) {
 
 app.get('/movie', handleGetMovie);
 
-const PORT = 8000;
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
+
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
