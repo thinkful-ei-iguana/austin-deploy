ce6df138f4a35ad846984b756a589b60e1571202
AustinT33 <austintumlinson@gmail.com>
2019-12-06 14:19:44 -0600

    fixed

--- app.js
+++ app.js
@@ -64,8 +64,4 @@ app.use((error, req, res, next) => {
   res.status(500).json(response)
 })
 
-const PORT = process.env.PORT || 8000;
-
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
-});
+module.exports = app
